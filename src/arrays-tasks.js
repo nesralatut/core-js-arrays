3f0f6b428de92769ee206abc6e5ae81f2c865578
Nina Rozova <127274141+nesralatut@users.noreply.github.com>
2024-01-05 15:43:48 +0300

    feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,16 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const length = end - start + 1;
+
+  function getValue(index) {
+    return index + start;
+  }
+
+  const array = Array.from(Array(length).keys(), getValue);
+
+  return array;
 }
 
 /**
