1b0123779ab9f80f3deab4f8a8284bc2c8bbe2ff
Nina Rozova <127274141+nesralatut@users.noreply.github.com>
2024-01-25 08:48:00 +0300

    feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -523,8 +523,15 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const result = [];
+
+  arr1.map((element) => {
+    if (arr2.includes(element)) result.push(element);
+    return element;
+  });
+
+  return result;
 }
 
 /**
